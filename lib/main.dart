cd6d618582e4197bece35babaef28017c69f279b
Andrea Fabbricatore <andy.fabb10@gmail.com>
2022-06-27 17:33:31 +0200

    added map configurations so that implementation can begin, added firebase to graddle for android usage

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await dotenv.load(fileName: "assets/.env");
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
--- lib/screens/createeventscreen.dart
+++ lib/screens/createeventscreen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
 
 class CreateEventScreen extends StatelessWidget {
   const CreateEventScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          "Create Event",
+          style: TextStyle(
+              color: Color.fromARGB(255, 255, 48, 117),
+              fontWeight: FontWeight.bold,
+              fontSize: 30),
+        ),
+        backgroundColor: Colors.white,
+        shadowColor: Colors.white,
+        elevation: 0.0,
+        automaticallyImplyLeading: false,
+      ),
+    );
   }
 }
