6a3c688bef4b403a84c53e495992d42bbf186015
Andrea Fabbricatore <andy.fabb10@gmail.com>
2022-09-02 22:46:57 +0200

    finished merging with notifications branch

---
+++ functions/lib/index.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sendToDevice = void 0;
+/* eslint-disable max-len */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+const db = admin.firestore();
+const fcm = admin.messaging();
+exports.sendToDevice = functions.firestore.document("updates/{id}").onCreate(async (snapshot) => {
+    const noti = snapshot.data();
+    const querySnapshot = await db.collection("users").doc(noti.target).collection("tokens").get();
+    const tokens = querySnapshot.docs.map((snap) => snap.id);
+    const payload = {
+        notification: {
+            title: noti.title,
+            body: noti.description,
+        },
+    };
+    await db.collection("updates").doc(snapshot.id).delete();
+    return fcm.sendToDevice(tokens, payload);
+});
+//# sourceMappingURL=index.js.map
